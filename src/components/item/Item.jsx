f833b744e6b868df5726123cb2522215ead79c5f
Vic <victorialau.dg@gmail.com>
2023-01-07 02:46:44 -0300

    Incorporación de Material UI y prueba de CSS modular

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,13 +1,13 @@
-import "./Item.css"
 import {Link} from "react-router-dom"
+import styles from "./Item.module.css"
 
 const Item = ({element}) => {
   return (
-    <div className="card" key={element.name}>
-        <h2 className="name">{element.name}</h2>
+    <div className={styles.card} key={element.name}>
+        <h2 className={styles.name}>{element.name}</h2>
         <img src={element.img} alt="" />
-        <h3 className="price">${element.price}</h3>
-        <p className="category">Planta de {element.category}</p>
+        <h3 className={styles.price}>${element.price}</h3>
+        <p className={styles.category}>Planta de {element.category}</p>
         <Link to={`/item/${element.id}`}>Ver detalle</Link>
     </div>
   )
